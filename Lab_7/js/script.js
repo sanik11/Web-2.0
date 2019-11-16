e3c28bb6cac5dceaed71c71736df696c28c10bab
sanik11 <49643350+sanik11@users.noreply.github.com>
2019-11-16 20:25:19 +0200

    Add files via upload

---
+++ Lab_7/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.gallery').slick({
+
+      lazyLoad: 'ondemand',
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      });
+  });
