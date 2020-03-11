e7c80120551829ccb6f5938b4fec4f96a237c123
sanik11 <49643350+sanik11@users.noreply.github.com>
2020-03-11 14:33:30 +0200

    Add files via upload

---
+++ Object/object.js
@@ -0,0 +1,24 @@
+var city1 = {};
+
+city1.name = "city N";
+city1.population = 10 ** 7;
+city1.getName = function() {
+    return this.name;
+}
+city1.exportStr = function() {
+    return "name = " + this.name + "\npopulation = " + this.population + "\n";
+}
+
+
+var city2 = {
+    "name": "city M",
+    "population": 10 ** 7,
+    getName() {
+        return this.name;
+    },
+    exportStr() {
+        return "name = " + this.name + "\npopulation = " + this.population + "\n";
+    }
+};
+
+console.log("sdf")
